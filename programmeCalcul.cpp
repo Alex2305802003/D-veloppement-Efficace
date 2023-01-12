200c58314829f4096d5a75409cb86127ab8015de
Alex2305802003 <78276248+Alex2305802003@users.noreply.github.com>
2023-01-12 12:36:38 +0100

    Add files via upload

---
+++ programmeCalcul.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+stack<int> nombres;
+
+void push(int num) {
+    nombres.push(num);
+}
+
+int pop() {
+    int num = nombres.top();
+    nombres.pop();
+    return num;
+}
+
+int top() {
+    return nombres.top();
+}
+/// @brief Sert à calculer en fonction des opérateurs grâce au switch case
+/// @param operateur 
+/// @return l'opération entre les 2 opérandes et l'opérateur
+int calcul(char operateur) {
+    int num1, num2;
+    num2 = pop();
+    num1 = pop();
+    switch (operateur) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            if (num2 == 0){
+                cout << "Erreur de calcul : impossible de diviser par 0"<< endl;
+            }
+            else
+            return num1 / num2;
+        default:
+            cout << "Erreur: opérateur inconnu" << endl;
+            return 0;
+    }
+}
+/// @brief Analyse la chaine des opérandes et opérateurs
+/// @param chaine 
+/// @return le calcul des opérandes et opérateurs grâce au push de la fonction calcul en fonction du caractère (opérateur)
+int eval(string chaine) {
+    int nombre = 0;
+    bool lecture_nombre = false;
+    for (int i = 0; i < chaine.length(); i++) {
+        char c = chaine[i];
+        if (c == ' ') {
+            if (lecture_nombre) {
+                push(nombre);
+                nombre = 0;
+                lecture_nombre = false;
+            }
+        } else if (isdigit(c)) {
+            lecture_nombre = true;
+            nombre = nombre * 10 + c - '0';
+        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+            if (lecture_nombre) {
+                push(nombre);
+                nombre = 0;
+                lecture_nombre = false;
+            }
+            push(calcul(c));
+        } else {
+            cout << "Erreur: caractère inconnu" << endl;
+            return 0;
+        }
+    }
+    if (lecture_nombre) {
+        push(nombre);
+    }
+    return top();
+}
+
+int main() {
+    string chaine;
+    cout << "Entrez une expression en notation polonaise inversée, mettre des espaces entre les opérateurs et les opérandes sinon ça ne marche pas : ";
+    getline(cin, chaine);
+    cout << "Résultat: " << eval(chaine) << endl;
+    return 0;
+}
